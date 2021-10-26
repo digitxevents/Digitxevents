0f4de89626136fb1b4faed439d35341a9d2acba2
Prosen <prosenbiswas224@gmail.com>
2021-10-27 01:55:57 +0600

    Contact us

---
+++ contact-us/contact-content.php
@@ -0,0 +1,70 @@
+<div class="contact-us">
+	<div class="container py-5 px-4">
+		<div class="row justify-content-between">
+			<div class="contact-left">
+				<div class="contact-image">
+					<img src="img/contact-us.png" alt="">
+				</div>
+				
+				<div class="contact-details">
+					<div class="d-flex align-items-center contact-address">
+						<div>
+							<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
+						</div>
+						<div class="contact-details-text">
+							44-70 21st Street #3086<br>Long Island City,<br>NY 11101<br>United States
+						</div>
+					</div>
+
+					<div class="d-flex contact-phone">
+						<div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/></svg>
+						</div>
+						<div class="contact-details-text">
+							+1 (516) 847-4880
+						</div>
+					</div>
+
+					<div class="d-flex contact-email">
+						<div>
+						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z"/></svg>
+						</div>
+						<div class="contact-details-text">
+							admin@digitxevents.com
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="contact-right">
+				<form>
+					<h1 class="title text-center mb-4">Get in touch</h1>
+					<!-- Name -->
+					<div class="form-group position-relative">
+						<label for="formName" class="d-block">
+							<i class="icon" data-feather="user"></i>
+						</label>
+						<input type="text" id="formName" class="form-control form-control-lg thick" placeholder="Name">
+					</div>
+
+					<!-- E-mail -->
+					<div class="form-group position-relative">
+						<label for="formEmail" class="d-block">
+							<i class="icon" data-feather="mail"></i>
+						</label>
+						<input type="email" id="formEmail" class="form-control form-control-lg thick" placeholder="Email">
+					</div>
+
+					<!-- Message -->
+					<div class="form-group message">
+						<textarea id="formMessage" class="form-control form-control-lg" rows="7" placeholder="Message"></textarea>
+					</div>
+				
+					<!-- Submit btn -->
+					<div class="text-center submit-button">
+						<button type="submit" class="btn btn-primary" tabIndex="-1">Send Message</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
