227ee24e82da078359b4203abe872a2e43d71d5c
Prosen <prosenbiswas224@gmail.com>
2021-11-03 00:48:48 +0600

    Blogs

---
+++ blogs/blogs-content.php
@@ -0,0 +1,55 @@
+<div class="dxe-blogs">
+    <div class="container py-5 px-4">
+        <h1 class="text-center mb-5 section-title">Our Blogs</h1>
+        <div class="row align-items-center justify-content-between">
+            <div class="card card-calc mb-4">        
+                <div class="card-body">
+                    <img src="https://thehackernews.com/new-images/img/a/AVvXsEgly3ynZqRUgIk8eaQI3ACZ6rpkFw0RZ40CXxAvusm_imwL1dgvJ3jiKVwoxFqYRtOL12IrCe1580PpFo8dxP3nXUpVMg2PSt75raRc6Aliqp3juIRilqztTEQSSdBnsY7tmAS_RFHr847lXP9JcajCCgGXlhhR-J5PNTyorMZ3a-xlC5UcNBF_EfsQ" class="card-img-top" alt="...">
+                    <h5 class="card-title fw-bold mt-3">Alert! Hackers Exploiting GitLab Unauthenticated RCE Flaw in the Wild</h5>
+                    <p class="card-text">A now-patched critical remote code execution (RCE) vulnerability in GitLab's web interface has been...<span><a href="">Read More</a></span></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card card-calc mb-4">        
+                <div class="card-body">
+                    <img src="https://thehackernews.com/new-images/img/a/AVvXsEju1hDqvJzDbl_FoN-1W7hcEqheMkmOFww4j4cUuop8OkkIriM_GZ5d2XfjNccL4Z7GBwD9uM7vyT2FU3URD4KOrwf9XksnHhSYBb9U-pHU62E-t8qggP94_EjmSXk4pN_cLMCjhLsGPyqk9VlcjZV7tRLOqMFKZ5H0V-B8aVAJVS9OvqVonXm_5W1G" class="card-img-top" alt="...">
+                    <h5 class="card-title fw-bold mt-3">Google to Pay Hackers $31,337 for Exploiting Patched Linux Kernel Flaws</h5>
+                    <p class="card-text">Google on Monday announced that it will pay security researchers to find exploits using vulnerabilities...<span><a href="">Read More</a></span></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card card-calc mb-4">        
+                <div class="card-body">
+                    <img src="https://thehackernews.com/new-images/img/a/AVvXsEizEp4OQW4wDSNAwvq0uto9BsVPAPqFrpSplKCFNc2FLYULJxOlzJJKRdvTsocAUdVK6Q7iMhb33WLc_9quIR5tIy8zXmcMA3QRMLJ-bzkUls4r_8vldCzMjt1r7qtqwnyJ7HU4MD7ic5vaRrthteD9KKY8XDReyeezNrRpChpSOXhEFwWYQMP9iuuy" class="card-img-top" alt="...">
+                    <h5 class="card-title fw-bold mt-3">Critical Flaws Uncovered in Pentaho Business Analytics Software</h5>
+                    <p class="card-text">Multiple vulnerabilities have been disclosed in Hitachi Vantara's Pentaho Business Analytics...<span><a href="">Read More</a></span></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card card-calc mb-4">        
+                <div class="card-body">
+                    <img src="https://thehackernews.com/images/-B408JpCRuTI/YQJwp5H9IoI/AAAAAAAADYI/gfcgYxRJ_RU3A-_wOhcueyMd2RF44oPXwCLcBGAsYHQ/s0/bec-attack.jpg" class="card-img-top" alt="...">
+                    <h5 class="card-title fw-bold mt-3">New 'Trojan Source' Technique Lets Hackers Hide Vulnerabilities in Source Code</h5>
+                    <p class="card-text">A novel class of vulnerabilities could be leveraged by threat actors to inject visually deceptive malware in...<span><a href="">Read More</a></span></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card card-calc mb-4">        
+                <div class="card-body">
+                    <img src="https://thehackernews.com/new-images/img/a/AVvXsEgzzEoJUu02JxXkUiOjHKPfrf-9JKilgjOh7CBoz96LPC43w9seVHKTSoLy_m2or529Jyr2KVWmNTmQvdKVjjHiO6mGEMFbmz5e0wgBITNgPA41DJdGNXpIXNj1FlZteRR8x3IeUyU2nsSJKC6hyvtqgmKotsnfhy8p7VdaUO4vJkPdkzs-_MV2QIE1" class="card-img-top" alt="...">
+                    <h5 class="card-title fw-bold mt-3">Researchers Uncover 'Pink' Botnet Malware That Infected Over 1.6 Million Devices</h5>
+                    <p class="card-text">Cybersecurity researchers disclosed details of what they say is the "largest botnet" observed in the...<span><a href="">Read More</a></span></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            <div class="card card-calc mb-4">        
+                <div class="card-body">
+                    <img src="https://thehackernews.com/new-images/img/a/AVvXsEijPT-bI7vc3UChYFKfLwTMUBBW6ypkrh5_FDH6m6g3dRSwslGZXX4mkhUW0MhaOzbMNC30krSm986swlhW3oXtqJ-X9qe8noCiL4QWjc5ga6gpdjrhvcx06MCSFaLo6PV9aTa5_Lu5hD5Kdgi-iDRS2lMJKrXFjijf_I_2RgwqKEEYacUZlKpnB-Ep" class="card-img-top" alt="...">
+                    <h5 class="card-title fw-bold mt-3">Police Arrest Suspected Ransomware Hackers Behind 1,800 Attacks Worldwide</h5>
+                    <p class="card-text">12 people have been detained as part of an international law enforcement operation for orchestrating...<span><a href="">Read More</a></span></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
